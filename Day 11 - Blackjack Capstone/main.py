20d2819cdc369b8b44b9b31c9e0b983347b81842
joshua.m.laubach <laubachj@leidos.com>
2022-11-29 10:33:37 -0500

    Completed Blackjack game.

--- Day 11 - Blackjack Capstone/main.py
+++ Day 11 - Blackjack Capstone/main.py
@@ -7,8 +7,6 @@ deck = ["2", "2", "2", "2", "3", "3", "3", "3", "4", "4", "4", "4", "5", "5", "5
 "7", "7", "7", "7", "8", "8", "8", "8", "9", "9", "9", "9", "10", "10", "10", "10", "J", "J", "J", "J", 
 "Q", "Q", "Q", "Q", "K", "K", "K", "K", "A", "A", "A", "A"]
 
-deal_track = 51
-
 def deck_shuffle(main_deck):
     random.shuffle(main_deck)
     return main_deck
@@ -35,17 +33,8 @@ def start_score(hand):
             score += int(c)
     return score
 
-def player_turn(start_score, main_deck, cc):
-    score = start_score
-    choice = "y"
-    choice = input("Type 'y' to HIT and get another card, and type 'n' to PASS: ")
-    while choice == "y":
-        new_card = main_deck[cc]
-        choice = input("Type 'y' to HIT and get another card, and type 'n' to PASS: ")
-
-
-
 deck_shuffle(deck)
+deal_track = 51
 starting_deal = deal(deck, deal_track)
 
 play_hand = [deal(deck, deal_track)[0], deal(deck, deal_track)[1]]
@@ -56,5 +45,63 @@ comp_score = start_score(comp_hand)
 print(f"Your cards: {play_hand}, current score: {play_score}")
 print(f"Computers first card: {comp_hand[0]}")
 
-player_turn(play_score, deck, deal_track)
+deck_cycle = 0
+current_card = deck[48 - deck_cycle]
+p_score = play_score
+c_score = comp_score
+card_value = 0
+choice = "y"
+choice = input("Type 'y' to HIT and get another card, and type 'n' to PASS: ")
 
+while choice == "y":
+    deck_cycle += 1
+    if current_card == 'A':
+        ace_choice = int(input("Do you want the ace to be a 1 or 11? Type '1' or '11' to choose: "))
+        if ace_choice == 1:
+            card_value = 1
+        else: card_value = 11
+    elif current_card == 'J':
+        card_value = 10
+    elif current_card == 'Q':
+        card_value = 10
+    elif current_card == 'K':
+        card_value = 10
+    else: card_value = int(current_card)
+
+    if (p_score + card_value) > 21:
+        print("You went over 21 and busted. You lose!")
+        quit()
+    else:
+        p_score += card_value
+        play_hand.append(current_card)
+        print(f"Your cards: {play_hand}, current score: {p_score}")
+        print(f"Computer's first card: {comp_hand[0]} ")
+        choice = input("Type 'y' to HIT and get another card, and type 'n' to PASS: ")
+
+while c_score < 17:
+    deck_cycle += 1
+    if current_card == 'A':
+        card_value = 1
+    elif current_card == 'J':
+        card_value = 10
+    elif current_card == 'Q':
+        card_value = 10
+    elif current_card == 'K':
+        card_value = 10
+    else: card_value = int(current_card)
+
+    c_score += card_value
+    play_hand.append(current_card)
+
+print(f"Your final hand: {play_hand}, final score: {p_score}")
+print(f"Computer's final hand: {comp_hand}, final score: {c_score}")
+
+if c_score > 21:
+    print("The computer busted. You Win!!!")
+elif c_score == p_score:
+    print("It's a Draw.")
+elif c_score > p_score:
+    print("You Lose!!!")
+else: print("You Win!!!")
+
+play_again = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
