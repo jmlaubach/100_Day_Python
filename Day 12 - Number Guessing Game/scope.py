df5f971c26b0d806edb129d11566eda06fedc33d
joshua.m.laubach <laubachj@leidos.com>
2022-11-30 07:35:45 -0500

    Finish scope notes.

--- Day 12 - Number Guessing Game/scope.py
+++ Day 12 - Number Guessing Game/scope.py
@@ -2,15 +2,21 @@
 
 enemies = 1
 
+# All uppercase naming convention is for global constant, variables that will never change
+PI = 3.14
+
 def increase_enemies():
     # Below line will pull the global enemies variable into the function to be used/modified
     # global enemies
     enemies += 2
     print(f"enemies inside function: {enemies}")
+    return enemies + 2
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
 
+# Suggested to avoid modifying global variables like above
+# Can use the return function instead
 
 # Can only call the nested drink_potion() function from within the game() function
 # because it was created within that function.
