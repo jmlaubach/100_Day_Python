663ac9ee657123a60c360d83a06a65deeb2e6243
jmlaubach <joshm.laubach@gmail.com>
2022-12-02 07:54:29 -0500

    Added very basic OOP notes.

---
+++ Day 16 - OOP/oop_notes.py
@@ -0,0 +1,19 @@
+
+# Object Oriented Programming (OOP)
+
+# Object is made up of 'attributes' and 'methods'
+    # attributes are variables associated with an object, and attached to that object
+    # methods are functions that modeled objects can do and use
+
+# Think of it as like modeling real life objects
+# If you were to model a waiter and create an object:
+    # attributes are what the waiter has
+    # methods are what the waiter does
+
+# Class: The main type for a job in a program (car)
+# Objects: Individual objects generated from this main class blueprint (types of cars)
+# car = CarBlueprint()
+    # CarBlueprint is the class. First letter of each word is capitalized to differentiate it
+    # car is the object, created from the CarBlueprint() class
+
+
