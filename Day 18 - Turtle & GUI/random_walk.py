f7a091462ae11a6a4be4fff6ea839aff5cb2943b
joshua.m.laubach <laubachj@leidos.com>
2023-01-13 08:22:54 -0500

    Add color randomization

--- Day 18 - Turtle & GUI/random_walk.py
+++ Day 18 - Turtle & GUI/random_walk.py
@@ -2,15 +2,24 @@ from turtle import *
 import random
 
 timmy = Turtle()
+colormode(255)
 timmy.speed(8)
 timmy.width(3)
 
 colors = ["red", "SeaGreen3", "yellow2", "SkyBlue1", "tan1"]
 directions = [90, 180, 270, 360]
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rand_color = (r, g, b)
+    return rand_color
+
+
 def random_walk(c, d):
     while True:
-        timmy.color(random.choice(c))
+        timmy.color(random_color())
         timmy.right(random.choice(d))
         timmy.forward(10)
 
