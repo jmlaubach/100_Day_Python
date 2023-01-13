bc85f2c605044494a006f9a027385d3a8ca16770
joshua.m.laubach <laubachj@leidos.com>
2023-01-13 08:44:18 -0500

    Add spirograph script

---
+++ Day 18 - Turtle & GUI/spirograph.py
@@ -0,0 +1,32 @@
+from turtle import *
+import random
+
+timmy = Turtle()
+colormode(255)
+timmy.speed(0)
+timmy.width(2)
+
+colors = ["red", "SeaGreen3", "yellow2", "SkyBlue1", "tan1"]
+directions = [90, 180, 270, 360]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rand_color = (r, g, b)
+    return rand_color
+
+
+def spirograph():
+    spin = 0
+    while spin <= 360:
+        timmy.color(random_color())
+        timmy.circle(100)
+        timmy.right(5)
+        spin += 5
+
+        
+spirograph()
+
+screen = Screen()
+screen.exitonclick()
